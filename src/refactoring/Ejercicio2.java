6c84a8c2d990d92c4acb36e5295ae4866a119ad8
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2025-01-13 22:12:28 +0100

    Cambios Robert Esquerre

--- src/refactoring/Ejercicio2.java
+++ src/refactoring/Ejercicio2.java
@@ -4,43 +4,48 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 
-		char v1 = 'a', v2 = 'e', v3 = 'i', v4 = 'o', v5 = 'u';
+		final char v1 = 'a', v2 = 'e', v3 = 'i', v4 = 'o', v5 = 'u';
 		char[] vocales = { v1, v2, v3, v4, v5 };
 
 		char[] tabla = new char[20];
 
-		int[] f = new int[5];
+		int[] frecuencia = new int[5];
 
-		for (int x = 0; x < 20; x++) {
-			int r = (int) (Math.random() * 5);
-			tabla[x] = vocales[r];
+		for (int i = 0; i < 20; i++) {
+			int randomNum = (int) (Math.random() * 5);
+			tabla[i] = vocales[randomNum];
 		}
 
-		for (int x = 0; x < 20; x++) {
-			if (tabla[x] == 'a') {
-				f[0] = f[0] + 1;
-			} else if (tabla[x] == 'e') {
-				f[1] = f[1] + 1;
-			} else if (tabla[x] == 'i') {
-				f[2] = f[2] + 1;
-			} else if (tabla[x] == 'o') {
-				f[3] = f[3] + 1;
-			} else if (tabla[x] == 'u') {
-				f[4] = f[4] + 1;
+		for (int i = 0; i < 20; i++) {
+			switch (tabla[i]) {
+		    case v1:
+		        frecuencia[0]++;
+		        break;
+		    case v2:
+		        frecuencia[1]++;
+		        break;
+		    case v3:
+		        frecuencia[2]++;
+		        break;
+		    case v4:
+		        frecuencia[3]++;
+		        break;
+		    case v5:
+		        frecuencia[4]++;
 			}
 		}
 
 		System.out.println("Tabla de vocales aleatorias:");
-		for (int x = 0; x < 20; x++) {
-			System.out.print(tabla[x] + " ");
+		for (int i = 0; i < 20; i++) {
+			System.out.print(tabla[i] + " ");
 		}
 
 		System.out.println("\n\nFrecuencia de cada vocal:");
-		System.out.println("Vocal '" + v1 + "': " + f[0]);
-		System.out.println("Vocal '" + v2 + "': " + f[1]);
-		System.out.println("Vocal '" + v3 + "': " + f[2]);
-		System.out.println("Vocal '" + v4 + "': " + f[3]);
-		System.out.println("Vocal '" + v5 + "': " + f[4]);
+		System.out.println("Vocal '" + v1 + "': " + frecuencia[0]);
+		System.out.println("Vocal '" + v2 + "': " + frecuencia[1]);
+		System.out.println("Vocal '" + v3 + "': " + frecuencia[2]);
+		System.out.println("Vocal '" + v4 + "': " + frecuencia[3]);
+		System.out.println("Vocal '" + v5 + "': " + frecuencia[4]);
 	}
 
 }
